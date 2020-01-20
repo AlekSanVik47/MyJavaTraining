22fa4e0f65042fb2dfedf199da790820e93c22a0
Alexander Bozhko <qa_aleksvik@outlook.com>
2020-01-21 01:26:19 +0400

    corrections added Math.min (max)

--- src/Homework2/Homework2Task5.java
+++ src/Homework2/Homework2Task5.java
@@ -1,25 +1,23 @@
 package Homework2;
 
 
+import java.util.Arrays;
+
 public class Homework2Task5 {
     public static void main(String[] args) {
 
-    }
 
-    public static void isMoreOrLessArr(int max, int min) {
-        int[] arr = {2, 15, 3, 10, 0, 175, 199, 247};
-        max = arr[0];
-        min = arr[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-            if (arr[i] > max) {
-                max = arr[i];
 
-                if (arr[i] < min) {
-                    min = arr[i];
-                    System.out.println("maximum number  " + max + ", minimum number  " + min);
+
+        int[] arr = new int[]{2, 15, 3, 10, 0, 175, 199, 247};
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 1; i < arr.length; ++i) {
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+
                 }
-            }
+
+            System.out.println("maximum number  " + max + ", minimum number  " + min);
         }
     }
-}
